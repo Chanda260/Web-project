9f171be3d53e96534c8d155426cc0223940be8f8
Chanda260 <161143582+Chanda260@users.noreply.github.com>
2024-12-22 12:01:19 +0200

    separating the js and css code into separate files

---
+++ operations.js
@@ -0,0 +1,57 @@
+
+onload = alert("WELCOME TO CHANDA'S BASIC MINI CALCULATOR!")
+
+function add(){
+
+ var a,b,c,d,operator;
+ a = document.getElementById("first").value;
+ b = document.getElementById("second").value;
+ d = document.getElementById("result");
+ 
+ operator = document.getElementById("operator").value;
+
+ if(isNaN(a) || isNaN(b)){
+   d.innerHTML = "Please Enter Numbers";
+ }
+
+ else if(document.getElementById("first").value == "" && document.getElementById("second").value == ""){
+   alert("Please Input Your First And Second Number For Evaluation!!");
+ }
+
+ else if(operator === "add"){
+   c = Number(a) + Number(b);
+   d.innerHTML = "The result is "+ c;
+   d.style.borderBottomStyle = "groove";
+ }
+
+ else if(operator === "sub"){
+   c = Number(a) - Number(b);
+   d.innerHTML = "The result is "+ c;
+   d.style.borderBottomStyle = "groove";
+ }
+
+ else if(operator === "mul"){
+   c = Number(a) * Number(b);
+   d.innerHTML = "The result is "+ c;
+   d.style.borderBottomStyle = "groove";
+ }
+
+ else if(operator === "div"){
+   c = Number(a) / Number(b);
+   d.innerHTML = "The result is "+ c;
+   d.style.borderBottomStyle = "groove";
+ }
+
+ else if(operator === "sqrt"){
+   c = Number(a) * Number(a)
+   d.innerHTML = "The result is "+ c;
+   d.style.borderBottomStyle = "groove";
+ }
+}
+
+function reset(){
+ document.getElementById("first").value = "";
+ document.getElementById("second").value = "";
+ document.getElementById("result").innerHTML = "";
+ document.getElementById("result").style.borderBottomStyle = "none";
+}
